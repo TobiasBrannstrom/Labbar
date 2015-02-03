a23735159ebc0e1a3e3d11ccd245650f6a518eb4
TobiasBrannstrom <tobias.brannstrom@hotmail.se>
2015-02-03 20:54:04 +0100

    Kylskåp v1

---
+++ Kylskåp/Cooler.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kylskåp
+{
+    public class Cooler
+    {
+        private decimal _insideTemperature;
+        private decimal _targetTemperature;
+        private const decimal OutsideTemperature = 23.7M;
+
+        public bool DoorIsOpen { get; set; }
+
+        public decimal InsideTemperature
+        {
+            get
+            {
+                return _insideTemperature;
+            }
+            private set
+            {
+                if (value < 0m || value > 45m)
+                {
+                    throw new ArgumentException();                   
+                }
+                _insideTemperature = value;
+            }
+        }
+
+        public bool IsOn { get; set; }
+
+        public decimal TargetTemperature
+        {
+
+            get
+            {
+                return _targetTemperature;
+            }
+            private set
+            {
+                if (value < 0m || value > 20m)
+                {
+                    throw new ArgumentException();
+                }
+                _targetTemperature = value;
+            }
+
+        }
+
+        public Cooler()
+            : this(0m, 0m)
+        {
+        }
+        public Cooler(decimal insideTemperature, decimal targetTemperature)
+            : this(insideTemperature, targetTemperature, false, false)
+        {
+        }
+
+        public Cooler(decimal temperature, decimal targetTemperature, bool isOn, bool doorIsOpen)
+        {
+            InsideTemperature = temperature;
+            TargetTemperature = targetTemperature;
+            IsOn = isOn;
+            DoorIsOpen = doorIsOpen;
+        }
+
+        public void Tick()
+        {
+            // På med stängd dörr
+            if (IsOn == true && DoorIsOpen == false)
+            {
+                _insideTemperature -= 0.2m;
+
+                if (_insideTemperature <= 4)
+                {
+                    _insideTemperature = 4m;
+                } //  detta känns klumpigt men det ger rätt utskrivning
+            }
+
+            // På med öppen dörr
+            else if (IsOn == true && DoorIsOpen == true)
+            {
+                _insideTemperature += 0.2m;
+                if (_insideTemperature >= OutsideTemperature)
+                {
+                    _insideTemperature = OutsideTemperature;
+                }
+            }
+
+            // Av med öppen dörr
+            else if (IsOn == false && DoorIsOpen == true)
+            {
+                _insideTemperature += 0.5m;
+                if (_insideTemperature >= OutsideTemperature)
+                {
+                    _insideTemperature = OutsideTemperature;
+                } // detta känns klumpigt men det ger rätt utskrivning
+            }
+
+            // Av med stängd dörr
+            else if (IsOn == false && DoorIsOpen == false)
+            {
+                _insideTemperature += 0.1m;
+                if (_insideTemperature >= OutsideTemperature)
+                {
+                    _insideTemperature = OutsideTemperature;
+                }
+            }
+          
+        }
+
+        public override string ToString()
+        {
+            string on = IsOn ? "PÅ" : "AV";
+            string open = DoorIsOpen ? "Öppet" : "Stängt";
+
+            return String.Format("[{0}] : {1:F1}°C : ({2:F1}°C) - {3}",
+            on,
+            _insideTemperature,
+            _targetTemperature,
+            open
+            );
+        }
+    }
+}
---
+++ Kylskåp/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kylskåp
+{
+    class Program
+    {
+
+        private const string HorizontalLine = "==================================================";
+
+        static private void Main(string[] args)
+        {
+            // Test 1
+            Cooler coolerTest = new Cooler();
+            ViewTestHeader("Test 1:\nTestar standardkonstruktorn.");
+            Console.WriteLine(coolerTest.ToString());
+            Console.WriteLine();
+
+            // Test 2
+
+            Cooler coolerTest2 = new Cooler(24.5m, 4);
+            ViewTestHeader("Test 2:\nTestar konstruktorn med 2 parametrar, (24,5 och 4).");
+            Console.WriteLine(coolerTest2.ToString());
+            Console.WriteLine();
+
+            // Test 3
+
+            Cooler coolerTest3 = new Cooler(19.5m, 4, true, false);
+            ViewTestHeader("Test 3:\nTestar konstruktorn med 4 parametrar, (24,5 och 4, True och False).");
+            Console.WriteLine(coolerTest3.ToString());
+            Console.WriteLine();
+
+            // Test 4
+
+            Cooler coolerTest4 = new Cooler(5.3m, 4, true, false);
+            ViewTestHeader("Test 4:\nTest av påslaget kylskåp med metoden Tick. Dörren är stängd.\n");
+            Run(coolerTest4);
+
+            // Test 5
+            Cooler coolerTest5 = new Cooler(5.3m, 4, false, false);
+            ViewTestHeader("Test 5:\nTest av avslaget kylskåp med metoden Tick. Dörren är stängd.\n");
+            Run(coolerTest5);
+
+            // Test 6
+            Cooler coolerTest6 = new Cooler(10.2m, 4, true, true);
+            ViewTestHeader("Test 6:\nTest av påslaget kylskåp med metoden Tick. Dörren är öppen.\n");
+            Run(coolerTest6);
+         
+            // Test 7
+            Cooler coolerTest7 = new Cooler(19.7m, 4, false, true);
+            ViewTestHeader("Test 7:\nTest av avslaget kylskåp med metoden Tick. Dörren är öppen.\n");
+            Run(coolerTest7);
+
+            // Test 8
+            ViewTestHeader("Test 8:\nTestar att ett undantag kastas då innertemperaturen tilldelas ett felaktigt \nvärde.");
+            try
+            {
+                Cooler coolerTest8 = new Cooler(50m, 4);
+                Run(coolerTest8);
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage("Innertemperaturn är inte i intervallet 0 - 45°C.");
+            }
+
+            // Test 9
+            ViewTestHeader("Test 9:\nTestar att ett undantag kastas då måltemperaturen tilldelas ett felaktigt värde.");
+            try
+            {
+                Cooler coolerTest9 = new Cooler(15m, 26);
+                Run(coolerTest9);
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage("Måltemperaturen är inte i intervallet 0 - 20°C.");
+            }
+        }
+
+        static private void Run(Cooler cooler)
+        {
+            // Startvärde för testen
+            Console.WriteLine(cooler.ToString());
+
+            // Simulerar att kylskåpet går i 10 minuter
+            for (int i = 0; i < 10; i++)
+            {
+                cooler.Tick();
+                Console.WriteLine(cooler.ToString());
+            }
+            
+            Console.WriteLine();
+        }
+
+        static private void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message); // skriv ut felmeddelanden från catchsatserna i test 8 & 9
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        static private void ViewTestHeader(string header)
+        {
+            Console.WriteLine(HorizontalLine);
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(header); // skriv ut strängen medskickad från testerna
+            Console.ResetColor();
+        }
+    }
+}
