c5eb6df88f8708657e178b32a4471d8ff8c50f7d
TobiasBrannstrom <tobias.brannstrom@hotmail.se>
2015-02-23 11:28:54 +0100

    tillägg

---
+++ Solida Volymer/CircularCone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solida_Volymer
+{
+    public class CircularCone : Solid
+    {
+        public override double BaseArea
+        {
+            get
+            {
+                return Math.PI * RadiusSquared;
+            }
+        }
+        public override double SurfaceArea
+        {
+            get
+            {
+                return Math.PI * Radius * (Radius + Math.Sqrt(RadiusSquared + HeightSqaured));
+            }
+        }
+        public override double Volume
+        {
+            get
+            {
+                return (1.0 / 3.0) * Math.PI * RadiusSquared * Height;
+            }
+        }
+        public CircularCone(double radius, double height)
+            : base (radius, height)
+        {
+        }
+    }
+}
---
+++ Solida Volymer/Cylinder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solida_Volymer
+{
+    public class Cylinder : Solid
+    {
+        public override double BaseArea
+        {
+            get
+            {
+                return Math.PI * RadiusSquared;
+            }
+
+        }
+        public override double SurfaceArea
+        {
+            get
+            {
+                return 2.0 * Math.PI * Radius * (Height + Radius);
+            }
+        }
+        public override double Volume
+        {
+            get
+            {
+                return Math.PI * RadiusSquared * Height;
+            }
+        }
+
+        public Cylinder(double radius, double height)
+            : base (radius, height) 
+        {
+        }
+        
+    }
+}
